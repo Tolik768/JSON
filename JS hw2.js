d44885f518a2dd743b21854fe93a7a6d196ecd8c
Tolik768 <tolik768@mail.ru>
2022-06-28 15:19:46 +0300

    add comit

---
+++ JS hw2.js
@@ -0,0 +1,10 @@
+//console.log('hello')
+
+//console.log(true || false)
+//console.log(true && false || true && true)
+
+if (false || true ){
+    console.log('1')
+} else {
+    console.log('2')
+}
